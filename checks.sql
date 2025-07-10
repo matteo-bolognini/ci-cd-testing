889d526cd85a1d0d5f7478b90e14be1fa417bcda
Matteo Bolognini <90386956+matteo-bolognini@users.noreply.github.com>
2025-07-10 16:20:51 +0100

    Update checks.sql

--- checks.sql
+++ checks.sql
@@ -7,5 +7,5 @@ SELECT version FROM db_schema_information;
 -- test 3: count computers
 SELECT count(*) FROM computers;
 
--- test 3: count mobile_devices
+-- test 4: count mobile_devices
 SELECT count(*) FROM mobile_devices;
