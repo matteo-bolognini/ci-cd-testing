61b9fd0e61d0c0ff3c407ad74b2657bedf2990bf
Matteo Bolognini <90386956+matteo-bolognini@users.noreply.github.com>
2025-07-10 16:20:17 +0100

    Update checks.sql

--- checks.sql
+++ checks.sql
@@ -4,6 +4,8 @@ SELECT * FROM jss_server_url;
 -- test 2: count the version of Jamf Pro
 SELECT version FROM db_schema_information;
 
-
 -- test 3: count computers
 SELECT count(*) FROM computers;
+
+-- test 3: count mobile_devices
+SELECT count(*) FROM mobile_devices;
